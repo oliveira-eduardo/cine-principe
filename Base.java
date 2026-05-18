cb4c6a727876e18446a82d3a5ba32f3e434d4177
JoaoBelo23 <joao.belo.cs@gmail.com>
2026-05-18 16:46:26 -0300

    Feat: Abstract class Base

---
+++ Base.java
@@ -0,0 +1,37 @@
+public abstract class Base{
+    private String nome;
+    private int idade;
+    private String email;
+
+    public Base(String nome, int idade, String email){
+        this.nome = nome; this.idade = idade; this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    abstract public void adicionarUsuario(Base usuario);
+    abstract public void alterarUsuario(Base usuario);
+    
+}
