a19aca0f0b26ef3e68ff53ceb744d46c059e377d
MatheusLopesGuedes <matheuslopesguedes@gmail.com>
2026-05-20 16:59:49 -0300

    feat: Classe concreta Administrador

---
+++ src/Administrador.java
@@ -0,0 +1,54 @@
+public class Administrador extends Base implements GerenciaFilme {
+    private double salario;
+    private int ID;
+
+    public Administrador(String nome, int idade, String email, double salario, int ID){
+        super(nome, idade, email);
+        this.salario = salario;
+        this.ID = ID;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getId() {
+        return ID;
+    }
+
+    public void setId(int id) {
+        this.ID = id;
+    }
+
+    @Override
+    public void adicionarUsuario(Base usuario){
+        System.out.println("Usuário adicionado ao sistema.");
+    }
+
+    @Override
+    public void alterarUsuario(Base usuario){
+        System.out.println("Usuário alterado no sistema.");
+    }
+
+    public void excluirUsuario(){
+        System.out.println("Usuário excluído do sistema.");
+    }
+
+    @Override
+    public void incluirFilme(Filme filme){
+        System.out.println("Filme incluído no catálogo.");
+    }
+    @Override
+    public void excluirFilme(Filme filme){
+        System.out.println("Filme excluído do catálogo.");
+    }
+
+    @Override
+    public void alterarFilme(Filme filme){
+        System.out.println("Filme alterado.");
+    }
+}
