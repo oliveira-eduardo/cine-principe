2d289f8da33745216563c9a58fdece98191472df
JoaoBelo23 <joao.belo.cs@gmail.com>
2026-05-23 12:43:29 -0300

    Feat: organizando o projeto em pastas

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,3 +1,6 @@
+package model;
+import repository.GerenciaFilme;
+
 public class Funcionario extends Base implements GerenciaFilme {
 
     double salario;
@@ -14,23 +17,23 @@ public class Funcionario extends Base implements GerenciaFilme {
     public void setSalario(double salario) {
         this.salario = salario;
     }
-    
+    @Override
     public void adicionarUsuario(Base usuario) {
 
     }
-    
+    @Override
     public void alterarUsuario(Base usuario) {
 
     }
-
+    @Override
     public void incluirFilme(Filme filme) {
 
     }
-
+    @Override
     public void alterarFilme(Filme filme) {
 
     }
-
+    @Override
     public void excluirFilme(Filme filme) {
         
     }
