aa789a7c908b0263d3c19f947ed940dd7b586ead
unknown <Joao.00000855750@unicap.br>
2026-05-28 16:51:34 -0300

    Feat: control Salas e Tela Salas

--- src/gui/TelaSalas.java
+++ src/gui/TelaSalas.java
@@ -15,57 +15,59 @@ import javax.swing.UIManager;
 
 import com.formdev.flatlaf.FlatDarkLaf;
 
+import control.ControlSalas; 
 import model.Bilhete;
 import model.Sala;
 import service.Sessao;
 
 public class TelaSalas extends JFrame {
     private Bilhete bilheteSala;
-    private Sala [] salasCine;
-    private Sessao [] session;
-    public TelaSalas(Sala [] salas,Bilhete bilhete){
+    private Sala[] salasCine;
+    private Sessao[] session;
+    private ControlSalas controlador; 
+
+    public TelaSalas(Sala[] salas, Bilhete bilhete) {
         this.salasCine = salas;
         this.bilheteSala = bilhete;
+        this.controlador = new ControlSalas(this);
+        
         JFrame.setDefaultLookAndFeelDecorated(true);
-        try { //coloca o modo escuro direto
+        try { // coloca o modo escuro direto
             UIManager.setLookAndFeel(new FlatDarkLaf());
-            
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        //cria a janela base 
+        
         setTitle("Salas - Cinema POO");
         setSize(650, 480);
-        // mudar para DISPOSE_ON_CLOSE depois dos testes
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         inicializarComponentes();
     }
-    public TelaSalas(){
 
+    public TelaSalas() {
     }
-    private void inicializarComponentes(){
+
+    private void inicializarComponentes() {
         JPanel janela = new JPanel(new BorderLayout(5, 5));
         janela.setBorder(BorderFactory.createEmptyBorder(10, 20, 20, 20));
 
         JPanel parteSuperior = new JPanel(new BorderLayout());
         parteSuperior.setOpaque(false);
-        
 
-        //Titulo que aparece de forma centralizada na tela
+        // Titulo que aparece na tela
         JLabel titulo = new JLabel("Selecione uma sala", JLabel.CENTER);
         titulo.setFont(new Font("Arial", Font.BOLD, 16));       
 
-        JPanel painelLogout = new JPanel(new FlowLayout(FlowLayout.LEFT, 0,0));
+        JPanel painelLogout = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
         painelLogout.setOpaque(false);
 
         JButton Logout = new JButton("Deslogar da Conta");
-        Logout.setFont(new Font("Arial",Font.PLAIN, 12));
+        Logout.setFont(new Font("Arial", Font.PLAIN, 12));
 
+        
         Logout.addActionListener(e -> {
-            TelaPrincipal telaPrincipal = new TelaPrincipal();
-            telaPrincipal.setVisible(true);
-            this.dispose();
+            controlador.deslogar();
         });
         painelLogout.add(Logout);
 
@@ -74,59 +76,59 @@ public class TelaSalas extends JFrame {
 
         janela.add(parteSuperior, BorderLayout.NORTH);   
 
-        //Cria uma painel para colocas as salas
+        // Cria uma painel para colocar as salas
         JPanel salasGrade = new JPanel(new GridLayout(0, 4, 5, 15));
         salasGrade.setOpaque(false);
 
-        for(int i = 0; i<salasCine.length; i++){
-            //um loop para criar um botao para cada sala que não estiver vazia
-            if(salasCine[i] != null){
+        for (int i = 0; i < salasCine.length; i++) {
+            // um loop para criar um botao para cada sala que não estiver vazia
+            if (salasCine[i] != null) {
                 final int index = i;
-                //cria o botão da sala com o nome dela 
+                
+                // cria o botão da sala com o nome dela 
                 JButton botaoSala = new JButton(salasCine[i].getNomeDaSala());
                 botaoSala.setPreferredSize(new java.awt.Dimension(120, 60));
                 botaoSala.setFocusPainted(false);
                 botaoSala.setBackground(new Color(45, 48, 50));
                 botaoSala.setFont(new Font("Arial", Font.BOLD, 20));
 
-                botaoSala.addActionListener(e ->{
-                    // se clicar no botao sala vai para area das sessoe que mostra os filmes
-                    // salva no bilhete o index da sala
-                    this.setSession(salasCine[index].getSessoes());
-                    this.bilheteSala.setSala(salasCine[index]);
-                    //chama a funcao para mostrar os filmes
-                    TelaFilmes mostrarFilme = new TelaFilmes(this);
-                    // deixa visivel a tela para mostrar sessao
-                    mostrarFilme.setVisible(true);
-                    //fecha a tela de salas
-                    this.setVisible(false);
+                
+                botaoSala.addActionListener(e -> {
+                    controlador.selecionarSala(index);
                 });
-                //adiciona o botao de sala
+                
+                
                 salasGrade.add(botaoSala);
             }
         }
         
-        //aqui adiciona tudo na janela de Sala, cada botão criado
+        
         janela.add(salasGrade, BorderLayout.CENTER);
         add(janela);
     }
+
+   
     public Bilhete getBilheteSala() {
         return bilheteSala;
     }
+
     public void setBilheteSala(Bilhete bilheteSala) {
         this.bilheteSala = bilheteSala;
     }
+
     public Sala[] getSalasCine() {
         return salasCine;
     }
+
     public void setSalasCine(Sala[] salasCine) {
         this.salasCine = salasCine;
     }
+
     public Sessao[] getSession() {
         return session;
     }
+
     public void setSession(Sessao[] session) {
         this.session = session;
     }
-    
 }
