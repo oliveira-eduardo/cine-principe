2f078faa1ec8ab4d190dc72283b7112a1a4738ec
unknown <Joao.00000855750@unicap.br>
2026-05-28 16:53:11 -0300

    control salas real

---
+++ src/control/ControlSalas.java
@@ -0,0 +1,28 @@
+package control;
+import gui.TelaFilmes;
+import gui.TelaPrincipal;
+import gui.TelaSalas;
+
+public class ControlSalas {
+    private TelaSalas tela;
+
+    public ControlSalas(TelaSalas tela) {
+        this.tela = tela;
+    }
+
+    public void selecionarSala(int index) {
+
+        tela.setSession(tela.getSalasCine()[index].getSessoes());
+        tela.getBilheteSala().setSala(tela.getSalasCine()[index]);
+
+        TelaFilmes mostrarFilme = new TelaFilmes(tela);
+        mostrarFilme.setVisible(true);
+        tela.setVisible(false);
+    }
+
+    public void deslogar() {
+        TelaPrincipal telaPrincipal = new TelaPrincipal();
+        telaPrincipal.setVisible(true);
+        tela.dispose();
+    }
+}
